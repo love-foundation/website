2b698fb8c4d82190aa7f431a939feddc3844f2c8
Mirko <mn@honeypot.io>
2020-02-04 13:40:27 +0100

    Make API URL an env variable

--- src/directus.js
+++ src/directus.js
@@ -1,20 +1,26 @@
 import DirectusSDK from '@directus/sdk-js';
 
 export const directus = new DirectusSDK({
-  url: "https://directus.love-foundation.club/",
+  url: process.env.DIRECTUS_URL,
   project: "_",
   token: process.env.DIRECTUS_TOKEN
 });
 
+
 export async function fetchEvents() {
   try {
-    let raw = await directus.getItems("events");
+    let raw = await directus.getItems("events", {
+      fields: "name, poster.data.*",
+      filter: {
+        on_website: 1
+      }
+    });
     const events = raw.data
-    return events;
-
+    return events
   } catch(err) {
     console.log(err);
   }
 };
+
 
 
