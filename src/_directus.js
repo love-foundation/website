e3b09ad4e029668328bb2174f9c110b77e9f2a8d
Mirko <mn@honeypot.io>
2020-02-29 22:25:58 +0100

    Adding functions to query page collection and individual files

--- src/_directus.js
+++ src/_directus.js
@@ -39,5 +39,30 @@ export async function fetchEvent(passedId) {
 };
 
 
+export async function fetchPageContent(passedSlug) {
+  try {
+    let raw = await directus.getItems("pages", {
+      fields: "slug, has_content.*, has_content.test_file",
+      filter: {
+        slug: passedSlug
+      }
+    });
+    const pages = raw.data
+    return pages
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+export async function fetchFile(passedID) {
+  try {
+    let raw = await directus.getFile(passedID.toString())
+    const file = raw.data
+    return file
+  } catch(err) {
+    console.log(err);
+  }
+};
+
 
 
