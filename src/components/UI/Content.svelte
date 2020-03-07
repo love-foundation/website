6ffc8d8ec317310fe26dc9e6ac95bea2cdae3c9e
Mirko <mn@honeypot.io>
2020-03-07 14:28:17 +0100

    Adjust template to match bulma styles and allow for responsiveness

--- src/components/UI/Content.svelte
+++ src/components/UI/Content.svelte
@@ -1,35 +1,53 @@
 <script>
-export let text = '';
-export let imageOne = null;
-export let imageTwo = null;
-export let type = 'text_only';
-
-
+  export let text = "";
+  export let imageOne = null;
+  export let imageTwo = null;
+  export let type = "text_only";
 </script>
 
+<style lang="scss">
+  // section {
+  //   max-width: $content-width;
+  //   margin: 0 auto !important;
+  // }
+</style>
+
 <section>
-{#if type == 'text_only'}
-   {@html text}
-{:else if  type == 'image_left'}
-   <div>
-      <img src="{imageOne.data.full_url}" alt="">
-   </div>
-   <div>
-      {@html text}
-   </div>
-{:else if type == 'image_right'}
-   <div>
-      {@html text}
-   </div>
-    <div>
-      <img src="{imageOne.data.full_url}" alt="">
-   </div>
-{:else if type == 'double_image'}
-   <div>
-      <img src="{imageOne.data.full_url}" alt="">
-   </div>
-   <div>
-      <img src="{imageTwo.data.full_url}" alt="">
-   </div>
-{/if}
+  <div class="container columns is-vcentered">
+    {#if type == 'text_only'}
+      <div class="column">
+        {@html text}
+      </div>
+    {:else if type == 'image_left'}
+      <div class="column is-half">
+        <figure class="image">
+          <img src={imageOne.data.full_url} alt="" />
+        </figure>
+
+      </div>
+      <div class="column is-half">
+        {@html text}
+      </div>
+    {:else if type == 'image_right'}
+      <div class="column is-half">
+        {@html text}
+      </div>
+      <div class="column is-half">
+        <figure class="image">
+          <img src={imageOne.data.full_url} alt="" />
+        </figure>
+      </div>
+    {:else if type == 'double_image'}
+      <div class="column is-half">
+        <figure class="image">
+          <img src={imageOne.data.full_url} alt="" />
+        </figure>
+      </div>
+      <div class="column is-half">
+        <figure class="image">
+          <img src={imageTwo.data.full_url} alt="" />
+        </figure>
+      </div>
+    {/if}
+  </div>
 </section>
