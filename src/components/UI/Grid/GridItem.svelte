d9d1d9246093b07d1dc52a799cf97fcc80b871b9
Mirko <mn@honeypot.io>
2020-03-08 13:39:11 +0100

    WIP columns, don't wrap properly atm

---
+++ src/components/UI/Grid/GridItem.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let id = null;
+  export let imageUrl = "";
+  export let title = "";
+  export let hub = "";
+  export let index = null;
+
+  let cardClass = "";
+
+  if (index === 0) {
+    cardClass = "is-6";
+  } else if (index % 10 === 0 || (index + 1) % 10 === 0) {
+    cardClass = "is-6";
+  } else {
+    cardClass = "is-3";
+  }
+</script>
+
+<style lang="scss">
+  .card-header {
+    flex-wrap: wrap;
+    box-shadow: none;
+    text-align: center;
+  }
+  h3 {
+    width: 100%;
+    &.hub {
+      text-transform: capitalize;
+    }
+  }
+</style>
+
+<a rel="prefetch" href="/events/{id}" class="card column {cardClass}">
+  <div class="card-image">
+    <figure class="image is-3by4">
+      <img src={imageUrl} alt="{title} Poster" />
+    </figure>
+  </div>
+  <div class="card-header">
+    <h3 class="title card-header-title is-centered">{title}</h3>
+    <h3 class="hub card-header-title is-centered">{hub}</h3>
+  </div>
+</a>
--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -1,35 +1,33 @@
 <script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`events.json`).then(r => r.json()).then(events => {
-			return { events };
-		});
-	}
+  export function preload({ params, query }) {
+    return this.fetch(`events.json`)
+      .then(r => r.json())
+      .then(events => {
+        return { events };
+      });
+  }
 </script>
 
 <script>
-	export let events;
+  import GridItem from "../../components/UI/Grid/GridItem.svelte";
+  export let events;
 </script>
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
+<style lang="scss">
+
 </style>
 
 <svelte:head>
-	<title>Events</title>
+  <title>Events</title>
 </svelte:head>
 
-<h1>Recent events</h1>
-
-<ul>
-	{#each events as event}
-		<li>
-			<ul>
-				<li><a rel='prefetch' href="/events/{event.id}">{event.title}</a></li>
-				<li><img src="{event.imageUrls.full_url}" alt="{event.title} Poster"></li>
-			</ul>
-		</li>
-	{/each}
-</ul>
+<div class="columns is-multiline">
+  {#each events as event, i (event.id)}
+    <GridItem
+      id={event.id}
+      imageUrl={event.imageUrls.full_url}
+      title={event.title}
+      hub={event.hub}
+      index={i} />
+  {/each}
+</div>
