283ecc41e53aacc60e8ffad861d4371ea75c02e8
Mirko <mn@honeypot.io>
2020-03-15 13:12:35 +0100

    Adding the API call for the artists

--- src/_directus.js
+++ src/_directus.js
@@ -19,6 +19,24 @@ export async function fetchItems(collection = "", fields = "", filter = {}) {
   } catch (err) {
     console.log(err);
   }
+<<<<<<< HEAD
+=======
+};
+
+export async function fetchArtists() {
+  try {
+    let raw = await directus.getItems("artists", {
+      fields: "id, artist_name, image.data.*, current_location, type_of_art"
+    });
+    const artists = raw.data
+    return artists
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+
+>>>>>>> Adding the API call for the artists
 
 };
 
