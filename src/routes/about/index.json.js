3ac20f4d3c9e9d7f12737fc9cda401f0f3a60f31
Mirko <mn@honeypot.io>
2020-03-21 16:38:04 +0100

    Swap out content query

--- src/routes/about/index.json.js
+++ src/routes/about/index.json.js
@@ -1,36 +1,28 @@
-import { fetchFile, fetchPageContent } from '../../_directus';
+import { fetchItems } from '../../_directus';
 
 
 export async function get(req, res, next) {
-  const pageContent = await fetchPageContent("test");
+  const pageContent = await fetchItems("pages", "slug, content.*.*.*", {
+    slug: "test"
+  });
   const queriedContent = pageContent[0].content;
 
-  // queriedContent.map(async (content) => {
-  //   let output;
-  //   console.log(content)
-  //   if (content.image) {
-  //     let id = content.image;
-  //     output = await fetchFile(id);
-  //     content.image = output;
-  //   }
-  // })
-
-    if (queriedContent !== null) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(
-        JSON.stringify(
-          queriedContent.map(content => ({
-            id: content.id,
-            text: content.text,
-            type: content.type,
-            imageOne: content.image,
-            imageTwo: content.image_two
-          }))
-        )
-      );
-    } else {
-      next();
-    }
+  if (queriedContent !== null) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(
+      JSON.stringify(
+        queriedContent.map(content => ({
+          id: content.id,
+          text: content.text,
+          type: content.type,
+          imageOne: content.image,
+          imageTwo: content.image_two
+        }))
+      )
+    );
+  } else {
+    next();
+  }
 
 }
 
