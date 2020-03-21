2a0d12a35e31f6e5485e7d895b110b4e02f7c4c8
Mirko <19408385+moekify@users.noreply.github.com>
2020-03-21 18:59:09 +0100

    handle item limit properly and remove the fetchArtists function

--- src/_directus.js
+++ src/_directus.js
@@ -7,37 +7,19 @@ export const directus = new DirectusSDK({
 });
 
 
-export async function fetchItems(collection = "", fields = "", filter = {}) {
+export async function fetchItems(collection = "", fields = "", filter = {}, limit = -1) {
 
   try {
     let raw = await directus.getItems(collection, {
       fields: fields,
-      filter: filter
+      filter: filter,
+      limit: limit
     });
     const items = raw.data
     return items
   } catch (err) {
     console.log(err);
   }
-<<<<<<< HEAD
-=======
-};
-
-export async function fetchArtists() {
-  try {
-    let raw = await directus.getItems("artists", {
-      fields: "id, artist_name, image.data.*, current_location, type_of_art",
-      limit: 20
-    });
-    const artists = raw.data
-    return artists
-  } catch(err) {
-    console.log(err);
-  }
-};
-
-
->>>>>>> Adding the API call for the artists
 
 };
 
