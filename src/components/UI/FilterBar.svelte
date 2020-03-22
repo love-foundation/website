71173b203c6644273454ce69092117f3d6c0a48f
Mirko <19408385+moekify@users.noreply.github.com>
2020-03-22 20:17:44 +0100

    WIP Filter Bar

--- src/components/UI/FilterBar.svelte
+++ src/components/UI/FilterBar.svelte
@@ -6,11 +6,45 @@
   export let filterTwo = [];
   export let placeholderTwo = "";
 
+
+
   export let title = "";
 
   let selectedValueOne = undefined;
   let selectedValueTwo = undefined;
 </script>
+
+<style lang="scss">
+  .select {
+    --border: none;
+    --placeholderColor: black;
+    --inputFontSize: 3.6rem;
+    --height: 44px;
+    --itemIsActiveColor: #0597F2;
+    --itemHoverBG: #0597F2;
+  }
+  :global(input) {
+    font-family: $family-primary;
+    color: $black;
+    font-size: 36px;
+    font-size: 3.6rem;
+    cursor: pointer !important;
+    &::placeholder {
+      opacity: 1;
+    }
+  }
+    :global(.selectedItem) {
+      font-size: 36px !important;
+      font-size: 3.6rem !important;
+      color: $black;
+      overflow-x: visible !important;
+      width: auto;
+    }
+    :global(.selection) {
+        overflow-x: visible !important;
+        text-transform: capitalize !important;
+      }
+</style>
 
 <div class="topbar columns is-horizontal">
   <h2 class="column is-2">{title}</h2>
@@ -20,7 +54,8 @@
       <Select
         items={filterOne}
         placeholder={placeholderOne}
-        bind:selectedValueOne />
+        bind:selectedValueOne
+        on:select />
     </div>
   </div>
   <div class="column is-2">
@@ -28,7 +63,8 @@
       <Select
         items={filterTwo}
         placeholder={placeholderTwo}
-        bind:selectedValueTwo />
+        bind:selectedValueTwo
+        on:select />
     </div>
   </div>
   <div class="column is-2">
