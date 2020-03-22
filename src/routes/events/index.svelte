476b567c898dc6f636291572795828001c659392
Mirko <19408385+moekify@users.noreply.github.com>
2020-03-22 20:17:44 +0100

    WIP Filter Bar

--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -18,8 +18,13 @@
   import GridGroup from "../../components/UI/Grid/GridGroup.svelte";
   import { stores } from "@sapper/app";
   import FilterBar from "../../components/UI/FilterBar.svelte";
+<<<<<<< HEAD
   const { session } = stores();
 
+=======
+  import {onMount} from "svelte";
+  export let events;
+>>>>>>> WIP Filter Bar
 
   let events = $session.events;
   let eventsArray = events;
@@ -27,8 +32,14 @@
   let len;
   let categories = [];
   let hubs = [];
+  let filteredEvents;
+  let filtered = false;
+  let filter
 
-  eventsArray.map(event => {
+
+
+  onMount(() => {
+    eventsArray.map(event => {
     hubs = [...hubs, event.hub];
     if (event.category) {
       categories = [...categories, event.category];
@@ -38,13 +49,38 @@
 
   hubs = [...new Set(hubs)];
   categories = [...new Set(categories)];
-  for (let i = 0, len = eventsArray.length; i < len; i += 5) {
+
+  })
+
+for (let i = 0, len = eventsArray.length; i < len; i += 5) {
     eventGroups.push(eventsArray.slice(i, i + 5));
   }
+  console.log(eventsArray)
 
-  // function filterEvents(filter) {
+// $:  eventsArray.length = eventsArray.length ? () => {
+//     console.log(eventsArray.length)
+//    } : console.log("test")
 
-  // }
+$: if(filteredEvents) {
+  eventGroups = new Set();
+  for (let i = 0, len = filteredEvents.length; i < len; i += 5) {
+    eventGroups = [...eventGroups, filteredEvents.slice(i, i + 5)];
+  }
+}
+
+
+
+
+
+  function filterEvents(filter) {
+    console.log("Got called dude!")
+    console.log(filter)
+
+    filteredEvents = eventsArray.filter(e => e.hub === filter)
+    console.log(filteredEvents)
+    filtered = true
+
+  }
 </script>
 
 <style lang="scss">
@@ -62,10 +98,11 @@
   filterOne={hubs}
   filterTwo={categories}
   placeholderOne={'Location'}
-  placeholderTwo={'Genre'} />
+  placeholderTwo={'Genre'}
+  on:select={(data) => {filterEvents(data.detail.value)}}/>
 
 <div class="tile is-ancestor is-vertical">
-  {#each eventGroups as eventGroup, i}
+  {#each eventGroups as eventGroup, i }
     <GridGroup itemGroup={eventGroup} groupIndex={i} />
   {/each}
 </div>
