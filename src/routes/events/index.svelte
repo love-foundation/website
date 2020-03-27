07d43f98bdf91039ad56bf4831838eb07ca0fe5b
Mirko <19408385+moekify@users.noreply.github.com>
2020-03-27 11:34:40 +0100

    WIP for events filter, each filter works on their own, combination not yet, reset currently not well

--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -8,9 +8,9 @@
   // }
 
   export function preload(_, session) {
-    const events = session.events
+    const events = session.events;
 
-    return {events}
+    return { events };
   }
 </script>
 
@@ -18,13 +18,8 @@
   import GridGroup from "../../components/UI/Grid/GridGroup.svelte";
   import { stores } from "@sapper/app";
   import FilterBar from "../../components/UI/FilterBar.svelte";
-<<<<<<< HEAD
   const { session } = stores();
-
-=======
-  import {onMount} from "svelte";
-  export let events;
->>>>>>> WIP Filter Bar
+  import { onMount } from "svelte";
 
   let events = $session.events;
   let eventsArray = events;
@@ -34,52 +29,101 @@
   let hubs = [];
   let filteredEvents;
   let filtered = false;
-  let filter
-
-
+  let currentFilters = {};
 
   onMount(() => {
     eventsArray.map(event => {
-    hubs = [...hubs, event.hub];
-    if (event.category) {
-      categories = [...categories, event.category];
-    }
+      hubs = [...hubs, event.hub];
+      if (event.category) {
+        categories = [...categories, event.category];
+      }
+    });
 
+    hubs = [...new Set(hubs)];
+    categories = [...new Set(categories)];
   });
 
-  hubs = [...new Set(hubs)];
-  categories = [...new Set(categories)];
+  // $: eventGroups = filtered
+  //   ? () => {
+  //     console.log("here")
+  //       for (let i = 0, len = filteredEvents.length; i < len; i += 5) {
+  //         eventGroups = [...eventGroups, filteredEvents.slice(i, i + 5)];
+  //       }
+  //       return eventGroups
+  //     }
+  //   : eventsArray => {
+  //     console.log("there")
+  //     console.log(eventsArray)
+  //     let output;
 
-  })
+  //       return eventGroups
+  //     };
 
-for (let i = 0, len = eventsArray.length; i < len; i += 5) {
-    eventGroups.push(eventsArray.slice(i, i + 5));
+  // $:  eventsArray.length = eventsArray.length ? () => {
+  //     console.log(eventsArray.length)
+  //    } : console.log("test")
+
+  $: if (filtered) {
+    eventGroups = [];
+    for (let i = 0, len = filteredEvents.length; i < len; i += 5) {
+      eventGroups = [...eventGroups, filteredEvents.slice(i, i + 5)];
+    }
+  } else {
+    eventGroups = [];
+    for (let i = 0, len = eventsArray.length; i < len; i += 5) {
+      eventGroups = [...eventGroups, eventsArray.slice(i, i + 5)];
+    }
   }
-  console.log(eventsArray)
-
-// $:  eventsArray.length = eventsArray.length ? () => {
-//     console.log(eventsArray.length)
-//    } : console.log("test")
-
-$: if(filteredEvents) {
-  eventGroups = new Set();
-  for (let i = 0, len = filteredEvents.length; i < len; i += 5) {
-    eventGroups = [...eventGroups, filteredEvents.slice(i, i + 5)];
-  }
-}
-
-
-
-
 
   function filterEvents(filter) {
-    console.log("Got called dude!")
-    console.log(filter)
+       console.log(filter);
+    console.log(currentFilters);
+    if (filter.hub) {
+      currentFilters.hub = filter.hub;
+    } else {
+      currentFilters.category = filter.category;
+    }
+    console.log(currentFilters);
 
-    filteredEvents = eventsArray.filter(e => e.hub === filter)
-    console.log(filteredEvents)
-    filtered = true
+    filteredEvents = eventsArray.filter(e => {
+      if (currentFilters.hub != "" && currentFilters.category != "") {
+        return (
+          e.hub === currentFilters.hub && e.category === currentFilters.category
+        );
+      } else if (currentFilters.hub == "" && currentFilters.category != "") {
+        return e.category === currentFilters.category;
+      } else if (currentFilters.hub != "" && currentFilters.category == "") {
+        return e.hub === currentFilters.hub;
+      } else {
+        return true;
+      }
+    });
 
+    console.log(filteredEvents);
+    filtered = true;
+  }
+
+  function filterLocation(filter) {
+    console.log(filter);
+    filteredEvents = eventsArray.filter(e => e.hub === filter.hub);
+    filtered = true;
+  }
+
+  function filterGenre(filter) {
+    console.log(filter);
+    filteredEvents = eventsArray.filter(e => e.category == filter.category);
+    // if(filteredEvents) {
+    //   filteredEvents = filteredEvents.filter(e => e.category == filter);
+    // } else {
+
+    // }
+    filtered = true;
+  }
+
+  function reset() {
+    currentFilters = {};
+    console.log(currentFilters);
+    console.log("resetted")
   }
 </script>
 
@@ -99,10 +143,17 @@ $: if(filteredEvents) {
   filterTwo={categories}
   placeholderOne={'Location'}
   placeholderTwo={'Genre'}
-  on:select={(data) => {filterEvents(data.detail.value)}}/>
+  on:selectedOne={data => {
+    filterEvents(data.detail);
+  }}
+  on:selectedTwo={data => {
+    filterEvents(data.detail);
+  }}
+  on:clear={reset()} />
 
+<button on:click={filterEvents}>Filtered</button>
 <div class="tile is-ancestor is-vertical">
-  {#each eventGroups as eventGroup, i }
+  {#each eventGroups as eventGroup, i (Math.random())}
     <GridGroup itemGroup={eventGroup} groupIndex={i} />
   {/each}
 </div>
