5a0d4c55bd04feafe04115a77008688e3cced6c0
Mirko <19408385+moekify@users.noreply.github.com>
2020-03-28 17:38:04 +0100

    Added TextModule for Content component

---
+++ src/components/UI/ContentModules/TextModule.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let content;
+
+  let wysiwyg;
+  let layout;
+
+  wysiwyg = content.text;
+  layout = content.textLayout;
+
+  let classNames = "";
+  switch (layout) {
+    case "two_columns":
+      classNames = "column is-8 two-col";
+      break;
+    case "full_width":
+      classNames = "column is-8";
+      break;
+    case "one_column":
+      classNames = "column is-4";
+  }
+</script>
+
+<style lang="scss">
+  .two-col {
+    columns: 2;
+    column-fill: balance;
+  }
+</style>
+
+<section class="columns is-centered">
+  <div class={classNames}`>
+    {@html wysiwyg}
+  </div>
+</section>
