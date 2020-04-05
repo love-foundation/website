828d6dd7eb8b111f9fa8cad4cfba3ea60ae7f314
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-05 19:05:17 +0200

    Adding single artist query and page with details

--- src/routes/artists/[slug].json.js
+++ src/routes/artists/[slug].json.js
@@ -1,13 +1,28 @@
 import { fetchItems } from '../../_directus';
 
 export async function get(req, res, next) {
-  const artist = await fetchItems("artists","id, artist_name, image.data.*, current_location, type_of_art", {
+  const artist = await fetchItems("artists", "id, artist_name, image.data.*, current_location, type_of_art, events.events_id.*.*.*, soundcloud_url, facebook_url, level_of_involvement", {
     id: req.params.slug
   });
 
   if (artist !== null) {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(artist));
+    res.end(JSON.stringify(artist.map(a => ({
+      name: a.artist_name,
+      image: a.image.data.full_url,
+      status: a.level_of_involvement,
+      location: a.current_location,
+      category: a.type_of_art,
+      facebook: a.facebook_url,
+      soundcloud: a.soundcloud_url,
+      events: a.events.map(event => ({
+        id: event.events_id.id,
+        title: event.events_id.name,
+        imageUrl: event.events_id.poster ? event.events_id.poster.data.full_url : "test",
+        hub: event.events_id.hubs[0] ? event.events_id.hubs[0].hubs_id.city : null
+      }))
+    })
+    )));
   } else {
     next();
   }
--- src/routes/artists/[slug].svelte
+++ src/routes/artists/[slug].svelte
@@ -3,26 +3,115 @@
     const res = await this.fetch(`/artists/${params.slug}.json`);
     const data = await res.json();
 
-   	if (res.status === 200) {
-			return { artist: data[0] };
-		} else {
-			this.error(res.status, data.message);
-		}
-	}
+    if (res.status === 200) {
+      return { artist: data[0] };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
 </script>
-
 
 <script>
+  import HeroModule from "../../components/UI/ContentModules/HeroModule.svelte";
+  import GridItem from "../../components/UI/Grid/GridItem.svelte";
+
+
   export let artist;
+
+  let {
+    name,
+    image,
+    location,
+    category,
+    status,
+    soundcloud,
+    facebook,
+    events
+  } = artist;
+
+  let heroContent = {};
+
+  heroContent.image = image;
 </script>
 
+<style lang="scss">
+  .info {
+    background: $light-grey;
+    width: 100vw;
+    margin-left: calc(-50vw + 50%);
+    .columns {
+      max-width: $content-width;
+      margin: 0 auto;
+      padding: 2em;
+    }
+  }
 
-<style>
-img {
-  width: 100%;
-  height: auto;
-}
+  .head {
+    background: $light-grey;
+    width: 100vw;
+    margin-left: calc(-50vw + 50%);
+    display: flex;
+    height: 100px;
+    border-bottom: 0.5px solid $dark-grey;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  .wrap {
+    flex-flow: wrap;
+  }
 </style>
 
-<h1>{artist.artist_name}</h1>
-<img src="{artist.image.data.full_url}" alt="">
+<svelte:head>
+  <title>Artists &mdash; {name}</title>
+</svelte:head>
+
+<HeroModule content={heroContent} />
+<div class="head">
+  <h1>{name}</h1>
+</div>
+<div class="info">
+  <div class="columns is-vertical is-multiline is-centered">
+    <div class="column is-4">
+      <h2>Hub:</h2>
+      <h2>{location}</h2>
+    </div>
+    <div class="column is-4">
+      <h2>Status:</h2>
+      <h2>{status}</h2>
+    </div>
+    <div class="column is-4">
+      <h2>Genre:</h2>
+      <h2>{category}</h2>
+    </div>
+
+    <div class="column is-4">
+      <h2>More:</h2>
+      {#if facebook}
+        <a href={facebook}>
+          <h2>Facebook</h2>
+        </a>
+      {/if}
+      {#if soundcloud}
+        <a href={soundcloud}>
+          <h2>Sundcloud</h2>
+        </a>
+      {/if}
+    </div>
+    <div class="column is-4" />
+  </div>
+</div>
+
+{#if events}
+<h2 class="pad--bottom--small pad--top--small">Events that this artist supported</h2>
+<div class="tile is-ancestor wrap">
+  {#each events as event}
+    <GridItem item={event} cardClass={'push--bottom--small is-4'} />
+  {/each}
+</div>
+{/if}
