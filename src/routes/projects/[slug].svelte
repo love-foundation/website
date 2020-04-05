116fb34b68bd720adceb5c142c37c537a2eb3b0b
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-05 15:44:54 +0200

    Adding single projects pages, no proper output

---
+++ src/routes/projects/[slug].svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export async function preload({ params, query }) {
+    const res = await this.fetch(`/projects/${params.slug}.json`);
+    const data = await res.json();
+
+   	if (res.status === 200) {
+			return { project: data[0] };
+		} else {
+			this.error(res.status, data.message);
+		}
+	}
+</script>
+
+
+<script>
+  export let project;
+</script>
+
+
+<style>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
+
+<h1>{project.name}</h1>
+<img src="{project.imageUrl}" alt="">
