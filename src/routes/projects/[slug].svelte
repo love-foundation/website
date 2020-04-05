ff2001967446686f45278e1d0955a52b029b658f
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-05 15:58:45 +0200

    Adding the content module on the single projects page

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -3,26 +3,34 @@
     const res = await this.fetch(`/projects/${params.slug}.json`);
     const data = await res.json();
 
-   	if (res.status === 200) {
-			return { project: data[0] };
-		} else {
-			this.error(res.status, data.message);
-		}
-	}
+    if (res.status === 200) {
+      return { project: data[0] };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
 </script>
-
 
 <script>
+  import Content from "../../components/UI/Content.svelte";
   export let project;
+
+  let { name, imageUrl, content, pillar } = project;
+  console.log(project);
 </script>
 
-
 <style>
-img {
-  width: 100%;
-  height: auto;
-}
+  img {
+    width: 100%;
+    height: auto;
+  }
 </style>
 
-<h1>{project.name}</h1>
-<img src="{project.imageUrl}" alt="">
+<svelte:head>
+  <title>Projects &mdash; {name}</title>
+</svelte:head>
+
+<h1>{name}</h1>
+<img src={imageUrl} alt="{name} image" />
+
+<Content queriedContent={content} />
