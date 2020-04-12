b06cd62c2968e74fbd7479714853de9111fc4e38
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-12 16:41:36 +0200

    Adjusting index page to display flex content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,20 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`index.json`)
+      .then(r => r.json())
+      .then(queriedContent => {
+        return { queriedContent };
+      });
+  }
+</script>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+<script>
+  import Content from "../components/UI/Content.svelte";
+	export let queriedContent;
+</script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Home</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<Content {queriedContent} />
