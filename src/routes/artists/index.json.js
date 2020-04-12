27b9d781188d1fe08a422115d0978f8544933532
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-12 17:38:46 +0200

    Shuffle artists and fixing some bugs with placeholders

--- src/routes/artists/index.json.js
+++ src/routes/artists/index.json.js
@@ -1,24 +1,23 @@
 import { fetchItems } from '../../_directus';
 
-
 export async function get(req, res, next) {
-  const artists = await fetchItems("artists","id, artist_name, image.data.*, current_location, type_of_art",{},20);
+	const artists = await fetchItems("artists", "id, artist_name, image.data.*, current_location, type_of_art", {}, 20);
 
-  if (artists !== null) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
+	if (artists !== null) {
+		res.setHeader('Content-Type', 'application/json');
+		res.end(
 			JSON.stringify(
 				artists.map(artist => ({
 					id: artist.id,
 					name: artist.artist_name,
-					imageUrls: artist.image ? artist.image.data.thumbnails[8].url : "placeholder_artists.jpeg",
+					imageUrl: artist.image ? artist.image.data.thumbnails[8].url : "placeholder_artists.jpeg",
 					location: artist.current_location,
 					category: artist.type_of_art
 				}))
 			)
 		);
-  } else {
+	} else {
 		next();
-  }
+	}
 }
 
