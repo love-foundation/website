bafae915415927c0364fdec6366a1d3758652625
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-13 15:26:42 +0200

    Swapping out id for slug for artists

--- src/routes/artists/[slug].json.js
+++ src/routes/artists/[slug].json.js
@@ -1,14 +1,15 @@
 import { fetchItems } from '../../_directus';
 
 export async function get(req, res, next) {
-  const artist = await fetchItems("artists", "id, artist_name, image.data.*, current_location, type_of_art, events.events_id.*.*.*, soundcloud_url, facebook_url, level_of_involvement, hero_background_color", {
-    id: req.params.slug
+  const artist = await fetchItems("artists", "id, slug, artist_name, image.data.*, current_location, type_of_art, events.events_id.*.*.*, soundcloud_url, facebook_url, level_of_involvement, hero_background_color", {
+    slug: req.params.slug
   });
 
   if (artist !== null) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(artist.map(a => ({
       name: a.artist_name,
+      slug: a.slug,
       image: a.image ? a.image.data.thumbnails[8].url : "placeholder_artists.jpeg",
       status: a.level_of_involvement,
       location: a.current_location,
@@ -18,6 +19,7 @@ export async function get(req, res, next) {
       heroColor: a.hero_background_color  || null,
       events: a.events.map(event => ({
         id: event.events_id.id,
+        slug: event.events_id.slug,
         title: event.events_id.name,
         imageUrl: event.events_id.poster ? event.events_id.poster.data.thumbnails[7] : "placeholder_artists.jpeg",
         hub: event.events_id.hubs[0] ? event.events_id.hubs[0].hubs_id.city : null
