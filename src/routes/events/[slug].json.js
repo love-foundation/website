a73a45d0e74f851591abfe4c93a8e0996c4577a7
Mirko <19408385+moekify@users.noreply.github.com>
2020-04-15 18:56:12 +0200

    Checking if there is any relations for a given artist or event

--- src/routes/events/[slug].json.js
+++ src/routes/events/[slug].json.js
@@ -18,12 +18,12 @@ export async function get(req, res, next) {
       imageUrl: e.poster ? e.poster.data.thumbnails[7].url : "placeholder_events.jpeg",
       artists_full: e.artists,
       heroColor: e.hero_background_color || null,
-      artists: e.artists.map(artist => ({
+      artists: e.artists ? e.artists.map(artist => ({
         id: artist.artists_id.id,
         slug: artist.artists_id.slug,
         name: artist.artists_id.artist_name,
         imageUrl: artist.artists_id ? artist.artists_id.image.data.thumbnails[3].url : "placeholder_artists.jpeg"
-      }))
+      })) : null
     })
 
 
