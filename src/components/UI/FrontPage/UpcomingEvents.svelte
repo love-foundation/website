73238918bb75b52071b3b5028f9909285c1b6eb2
Mirko <19408385+moekify@users.noreply.github.com>
2020-05-14 17:59:12 +0200

    Remove some templating and move it to the FrontPage to be more understandable

--- src/components/UI/FrontPage/UpcomingEvents.svelte
+++ src/components/UI/FrontPage/UpcomingEvents.svelte
@@ -1,15 +1,14 @@
- <script>
+<script>
   import GridItem from "../Grid/GridItem.svelte";
 
   export let upcomingEvents;
+</script>
 
- </script>
-
- <section>
-    <h2>Upcoming Events</h2>
-    <ul>
-    {#each upcomingEvents as upcomingEvent}
-       <GridItem item={upcomingEvent} showDate={true} cardClass={'push--bottom--small is-4'} />
-    {/each}
-    </ul>
-  </section>
+<div class="tile is-ancestor is-parent wrap">
+  {#each upcomingEvents as upcomingEvent}
+    <GridItem
+      item={upcomingEvent}
+      showDate={true}
+      cardClass={'push--bottom--small is-4'} />
+  {/each}
+</div>
