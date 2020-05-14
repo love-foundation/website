926816d021c72126cac8fea2de6c633d378a5905
Mirko <19408385+moekify@users.noreply.github.com>
2020-05-14 17:01:09 +0200

    Adding component for upcoming events and filtering session events for upcoming events to pass in

---
+++ src/components/UI/FrontPage/UpcomingEvents.svelte
@@ -0,0 +1,14 @@
+ <script>
+  import GridItem from "../Grid/GridItem.svelte";
+
+  export let upcomingEvents;
+ </script>
+
+ <section>
+    <h2>Upcoming Events</h2>
+    <ul>
+    {#each upcomingEvents as upcomingEvent}
+       <GridItem item={upcomingEvent} cardClass={'push--bottom--small is-4'} />
+    {/each}
+    </ul>
+  </section>
