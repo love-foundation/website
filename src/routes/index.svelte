0246398bc49f42039d8271d291ba3b9433bf9a98
Mirko <19408385+moekify@users.noreply.github.com>
2020-05-14 17:36:29 +0200

    Remove content query from index.svelte and insert information text about LF

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,4 @@
-<script context="module">
-  export function preload({ params, query }) {
-    return this.fetch(`index.json`)
-      .then(r => r.json())
-      .then(queriedContent => {
-        return { queriedContent };
-      });
-  }
-</script>
-
 <script>
-  import Content from "../components/UI/Content.svelte";
   import UpcomingEvents from "../components/UI/FrontPage/UpcomingEvents.svelte";
   import { stores } from "@sapper/app";
 
@@ -20,14 +9,31 @@
   let upcomingEvents = $session.events.filter(
     event => new Date(event.starttime) >= new Date()
   );
-
 </script>
 
 <svelte:head>
   <title>Home</title>
 </svelte:head>
 
-<Content {queriedContent} />
+<style lang="scss">
+  .centered {
+    text-align: center;
+  }
+</style>
+
+<section class="pad--bottom--small">
+  <div class="columns is-centered">
+    <div class="column is-8">
+      <h2 class="centered">
+        Love Foundation is an open network of artists and activists. Together we
+        organise projects to spread love, connect people and fundraise money for
+        water development projects in Kenya with our partners Viva con Agua and
+        Simavi.
+      </h2>
+    </div>
+  </div>
+
+</section>
 
 {#if upcomingEvents.length}
   <UpcomingEvents {upcomingEvents} />
