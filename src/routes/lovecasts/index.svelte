5e4801177f2f68e279c98ad6882842bb750d8ca3
Mirko <19408385+moekify@users.noreply.github.com>
2020-06-04 15:40:58 +0200

    Adjusting lovecast index for the new grid layout

--- src/routes/lovecasts/index.svelte
+++ src/routes/lovecasts/index.svelte
@@ -30,10 +30,6 @@
 </script>
 
 <style lang="scss">
-  .tile {
-    flex-flow: wrap !important;
-  }
-
   section {
     opacity: 0;
     transition: margin-top 1s cubic-bezier(0.4, 0.07, 0.32, 0.94),
@@ -53,8 +49,7 @@
   <section
     data-toggle-class="loaded"
     class:loaded={i < 1}
-    class:lozad={i >= 1}
-    class="tile is-ancestor is-vertical">
+    class:lozad={i >= 1}>
     <GridGroup itemGroup={lovecastGroup} groupIndex={i} lazy={i >= 1} />
   </section>
 {/each}
