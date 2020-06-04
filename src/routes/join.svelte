a9b90e335a61de83315d0e7e12e9e8472f4d8491
Mirko <19408385+moekify@users.noreply.github.com>
2020-06-04 16:44:37 +0200

    Add the join page

---
+++ src/routes/join.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Button from "../components/UI/Button.svelte";
+</script>
+
+<svelte:head>
+  <title>Love Foundation - Join the Family</title>
+</svelte:head>
+
+<h1 class="pad--bottom--small">Join the Family</h1>
+
+<div class="columns">
+  <div class="column is-4">
+    <h1>Hubs</h1>
+    <Button newTab={true} link={'mailto:hello@love-foundation.org'}>
+      Join our hubs
+    </Button>
+  </div>
+  <div class="column is-4">
+    <h1>Events</h1>
+    <Button newTab={true} link={'events'}>Join our events</Button>
+  </div>
+  <div class="column is-4">
+    <h1>Financial</h1>
+    <Button newTab={true} link={'mailto:hello@love-foundation.org'}>
+      Support us financially
+    </Button>
+  </div>
+</div>
+<Button newTab={true} link={'https://www.google.com'}>Hello World</Button>
