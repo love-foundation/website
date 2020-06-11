8b92bfe190dacdb7195d25237549c1afd9214157
Mirko <19408385+moekify@users.noreply.github.com>
2020-06-11 12:17:24 +0200

    Adding icons to the join the family page and styling according to design

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -2,6 +2,29 @@
   import Button from "../components/UI/Button.svelte";
 </script>
 
+<style lang="scss">
+  .bubble {
+    border-radius: 50%;
+    border: 10px solid $black;
+    margin: 5%;
+    position: relative;
+
+    .image {
+      object {
+        width: 100%;
+        padding: 20%;
+      }
+    }
+
+    h2 {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 10%;
+    }
+  }
+</style>
+
 <svelte:head>
   <title>Love Foundation - Join the Family</title>
 </svelte:head>
@@ -10,20 +33,34 @@
 
 <div class="columns">
   <div class="column is-4">
-    <h1>Hubs</h1>
+    <div class="bubble">
+      <figure class="image">
+        <object data="join_family.svg" type="image/svg+xml" title="Map" />
+      </figure>
+      <h2>Join a hub</h2>
+    </div>
     <Button newTab={true} link={'mailto:hello@love-foundation.org'}>
       Join our hubs
     </Button>
   </div>
   <div class="column is-4">
-    <h1>Events</h1>
+    <div class="bubble">
+      <figure class="image">
+        <object data="join_events.svg" type="image/svg+xml" title="Map" />
+      </figure>
+      <h2>Join an event</h2>
+    </div>
     <Button newTab={true} link={'events'}>Join our events</Button>
   </div>
   <div class="column is-4">
-    <h1>Financial</h1>
+    <div class="bubble">
+      <figure class="image">
+        <object data="join_financial.svg" type="image/svg+xml" title="Map" />
+      </figure>
+      <h2>Financial Hug</h2>
+    </div>
     <Button newTab={true} link={'mailto:hello@love-foundation.org'}>
       Support us financially
     </Button>
   </div>
 </div>
-<Button newTab={true} link={'https://www.google.com'}>Hello World</Button>
