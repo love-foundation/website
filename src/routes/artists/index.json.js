e9830055f99837d2ee1b5daeed49c9dbf112ef9f
Mirko <19408385+moekify@users.noreply.github.com>
2020-08-30 16:49:47 +0200

    Remove old json fixtures, adjust artist call to use fixtures in development

--- src/routes/artists/index.json.js
+++ src/routes/artists/index.json.js
@@ -1,25 +1,45 @@
 import { fetchItems } from '../../_directus';
 
+import fakeResponse from '../../../cypress/fixtures/artists.js'
+
+
 export async function get(req, res, next) {
 
-	const artists = await fetchItems("artists", "id, slug, artist_name, image.data.*, current_location, type_of_art", {});
+	let artists;
 
-	if (artists !== null) {
-		res.setHeader('Content-Type', 'application/json');
-		res.end(
-			JSON.stringify(
-				artists.map(artist => ({
-					id: artist.id,
-					slug: artist.slug,
-					name: artist.artist_name,
-					imageUrl: artist.image ? artist.image.data.thumbnails[8].url : "placeholder_artists.png",
-					location: artist.current_location,
-					category: artist.type_of_art
-				}))
-			)
-		);
+	const callApi = process.env.NODE_ENV === 'production';
+
+	if (callApi) {
+		const artists = await fetchItems("artists", "id, slug, artist_name, image.data.*, current_location, type_of_art", {});
+
+		if (artists !== null) {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(
+				JSON.stringify(
+					artists.map(artist => ({
+						id: artist.id,
+						slug: artist.slug,
+						name: artist.artist_name,
+						imageUrl: artist.image ? artist.image.data.thumbnails[8].url : "placeholder_artists.png",
+						location: artist.current_location,
+						category: artist.type_of_art
+					}))
+				)
+			);
+		} else {
+			next();
+		}
 	} else {
-		next();
+		artists = fakeResponse;
+		if (artists !== null) {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(
+				JSON.stringify(
+					artists.map(artist => ({
+						...artist
+					}))
+				)
+			);
+		}
 	}
 }
-
