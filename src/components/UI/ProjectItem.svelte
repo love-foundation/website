391c1921661f8103607d48090520a67a11065e27
Mirko <19408385+moekify@users.noreply.github.com>
2020-08-30 20:22:54 +0200

    Adjust styling  of projects index page

--- src/components/UI/ProjectItem.svelte
+++ src/components/UI/ProjectItem.svelte
@@ -9,22 +9,14 @@
     position: relative;
   }
   .project--card {
-     position: relative;
+    position: relative;
 
     h2 {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%,-50%);
       width: 100%;
-      text-align: center;
-      color: $white;
-      text-shadow: 2px 2px 2px $black;
     }
 
     img {
       width: 100%;
-    box-shadow: 7px 8px 5px -3px rgba(0, 0, 0, 0.5);
     }
   }
 </style>
@@ -32,6 +24,6 @@
 <a rel="prefetch" href="/projects/{slug}">
   <div class="project--card">
     <img src={imageUrl} alt="{name} Image" />
-    <h2>{name}</h2>
+    <h2 data-cy="projectTitle">{name}</h2>
   </div>
 </a>
--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -10,22 +10,52 @@
 
 <script>
   import ProjectItem from "../../components/UI/ProjectItem.svelte";
-  import { flip } from "svelte/animate"
+  import { flip } from "svelte/animate";
 
   export let projects;
 </script>
 
 <style lang="scss">
+  #projectList {
+    display: grid;
+    grid-template-areas:
+      "left"
+      "right";
+    grid-template-columns: 100%;
+    @include tablet {
+      grid-template-areas: "left" "right";
+      grid-template-columns: 50% 50%;
+      grid-column-gap: 5%;
+    }
+    .project {
+      padding: 15%;
+      grid-area: "left";
+      &.pull--top--medium {
+        margin-top: -100px;
+      }
+    }
+  }
 </style>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
 
-<div class="columns is-multiline is-vertical">
+<h1 class="centered" data-cy="pageTitle">Projects</h1>
+
+<p class="centered pad--bottom--small" data-cy="pageIntro">
+  Love Foundation is starting and supporting a variety of projects globally.
+</p>
+
+<ul id="projectList">
   {#each projects as project, i (project.id)}
-    <div animate:flip class="project column is-4">
+    <li
+      animate:flip
+      class="project"
+      class:push--bottom--large={i === 0 || (i + 1) % 2}
+      class:push--top--large={i % 2}
+      class:pull--top--medium={i > 1 && (i + 1) % 2}>
       <ProjectItem {project} />
-    </div>
+    </li>
   {/each}
-</div>
+</ul>
