8d9b18aa03696e14a0a8779bef2fa8712e8a9ded
Mirko <19408385+moekify@users.noreply.github.com>
2020-08-30 16:56:06 +0200

    Load fakeresponse for about when in development environment

--- src/routes/about/index.json.js
+++ src/routes/about/index.json.js
@@ -1,36 +1,57 @@
 import { fetchItems } from '../../_directus';
 
+import fakeResponse from '../../../cypress/fixtures/about.js'
 
 export async function get(req, res, next) {
-  const pageContent = await fetchItems("pages", "slug, content.*.*.*", {
-    slug: "about"
-  });
 
-  const queriedContent = pageContent[0].content;
+  let queriedContent;
 
-  if (queriedContent !== null) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify(
-        queriedContent.map(content => ({
-          id: content.id,
-          type: content.type,
-          details: {
-            text: content.text,
-            textLayout: content.text_layout,
-            imageOne: content.image,
-            imageTwo: content.image_two,
-            padding: content.distance_to_next,
-            captions: content.captions,
-            heroColor: content.hero_background_color  || null
-          }
+  const callApi = process.env.NODE_ENV === 'production';
 
-        }))
-      )
-    );
+  if (callApi) {
+    const pageContent = await fetchItems("pages", "slug, content.*.*.*", {
+      slug: "about"
+    });
+
+    queriedContent = pageContent[0].content;
+
+    if (queriedContent !== null) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify(
+          queriedContent.map(content => ({
+            id: content.id,
+            type: content.type,
+            details: {
+              text: content.text,
+              textLayout: content.text_layout,
+              imageOne: content.image,
+              imageTwo: content.image_two,
+              padding: content.distance_to_next,
+              captions: content.captions,
+              heroColor: content.hero_background_color || null
+            }
+
+          }))
+        )
+      );
+    } else {
+      next();
+    }
   } else {
-    next();
-  }
+    queriedContent = fakeResponse;
 
+    if (queriedContent !== null) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify(
+          queriedContent.map(content => ({
+            ...content
+            })
+          )));
+    } else {
+      next();
+    }
+  }
 }
 
