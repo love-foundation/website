8d277ac7e9093aeb18f6f6d8c5ff272ad079f7bb
Mirko <19408385+moekify@users.noreply.github.com>
2020-08-30 16:52:24 +0200

    Replace json fixtures with js files to allow importing

---
+++ cypress/fixtures/projects.js
@@ -0,0 +1,31 @@
+export default
+  [
+    {
+      "id": 2,
+      "name": "WASH Project Bihar",
+      "imageUrl": "https://minio.love-foundation.club/directus/pub/directus/originals/515b220d-a743-47fd-be63-c6de0d8a15fe.jpg",
+      "pillar": null,
+      "slug": "wash-project-bihar"
+    },
+    {
+      "id": 3,
+      "name": "Simavi Project in Kenya",
+      "imageUrl": "https://minio.love-foundation.club/directus/pub/directus/originals/0d42586a-3e23-4293-945b-27cb2449cdf7.jpg",
+      "pillar": null,
+      "slug": "simavi-project-in-kenya"
+    },
+    {
+      "id": 4,
+      "name": "Brazil Tour 2018",
+      "imageUrl": "https://minio.love-foundation.club/directus/pub/directus/originals/cc1e6824-cc1d-4e2e-b1b8-73310198c3c6.jpg",
+      "pillar": null,
+      "slug": "brazil-tour-2018"
+    },
+    {
+      "id": 5,
+      "name": "Viva con Agua de Sankt Pauli e.V.",
+      "imageUrl": "https://minio.love-foundation.club/directus/pub/directus/originals/cf9386f1-4acc-4977-ab56-ec0d7751fe29.jpg",
+      "pillar": null,
+      "slug": "viva-con-agua-de-sankt-pauli-e-v"
+    }
+  ]
