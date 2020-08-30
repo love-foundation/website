e1188ab92890f8f714c701a9aa303ed71464bf2a
Mirko <19408385+moekify@users.noreply.github.com>
2020-08-30 20:21:09 +0200

    Add tests for projects page and adjust dev environment to load projects from fixture

--- src/routes/projects/index.json.js
+++ src/routes/projects/index.json.js
@@ -1,25 +1,47 @@
 import { fetchItems } from '../../_directus';
 
+import fakeResponse from '../../../cypress/fixtures/projects.js'
+
+const callApi = process.env.NODE_ENV === 'production';
+
+let projects;
 
 export async function get(req, res, next) {
-  const projects = await fetchItems("projects", "id, name, main_image.*.*, pillar, slug", {});
 
-  if (projects !== null) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify(
-        projects.map(project => ({
-          id: project.id,
-          name: project.name,
-          imageUrl: project.main_image ? project.main_image.data.full_url : "placeholder_projects.jpeg",
-          pillar: project.pillar,
-          slug: project.slug
-        }))
-      )
-    );
+  if (callApi) {
+    projects = await fetchItems("projects", "id, name, main_image.*.*, pillar, slug", {});
+
+    if (projects !== null) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify(
+          projects.map(project => ({
+            id: project.id,
+            name: project.name,
+            imageUrl: project.main_image ? project.main_image.data.full_url : "placeholder_projects.jpeg",
+            pillar: project.pillar,
+            slug: project.slug
+          }))
+        )
+      );
+    } else {
+      next();
+    }
   } else {
-    next();
-  }
+    projects = fakeResponse;
 
+    if (projects !== null) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify(
+          projects.map(project => ({
+            ...project
+          }))
+        )
+      );
+    } else {
+      next();
+    }
+  }
 }
 
