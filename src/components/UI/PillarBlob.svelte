01a5ffa50a876c3e68345d18ec7756d2888f74c3
Mirko <19408385+moekify@users.noreply.github.com>
2020-10-31 15:42:10 +0100

    Make the pillar blob its own component

---
+++ src/components/UI/PillarBlob.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let pillar = "";
+</script>
+
+<style lang="scss">
+  .project--identifier {
+    margin-right: 10px;
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    &.water {
+      background-color: $water;
+    }
+    &.community {
+      background-color: $community;
+    }
+    &.sustainability {
+      background-color: $sustainability;
+    }
+    &.joy {
+      background-color: $joy;
+    }
+    &.love {
+      background-color: $love;
+      border: 2px solid $black;
+    }
+  }
+</style>
+
+
+<div data-cy="projectPillar" class={`project--identifier ${pillar}`} />
+
