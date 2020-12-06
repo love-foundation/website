92d4c57c10654a7f221500ebba7663715b880cbe
Mirko <19408385+moekify@users.noreply.github.com>
2020-12-06 16:25:06 +0100

    Add Matomo web analytics

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,11 @@
 <script context="module">
   export function preload({ params, query }, session) {
     return this.fetch(`events.json`)
-      .then(r => r.json())
-      .then(events => {
-        session.events = events;
-        return { events };
-      });
+      .then((r) => r.json())
+      .then((events) => {
+        session.events = events
+        return { events }
+      })
 
     // return this.fetch(`artists.json`)
     //   .then(r => r.json())
@@ -24,24 +24,24 @@
 </script>
 
 <script>
-  import Nav from "../components/UI/Nav.svelte";
-  import Footer from "../components/UI/Footer.svelte";
-  import { stores } from "@sapper/app";
+  import Nav from '../components/UI/Nav.svelte'
+  import Footer from '../components/UI/Footer.svelte'
+  import { stores } from '@sapper/app'
 
-  const { page } = stores();
+  const { page } = stores()
 
-  export let segment;
+  export let segment
 
-  let winHeight = 0;
-  let y = 0;
-  let progress = 0;
-  let docHeight = 0;
-  let mainClasses = "";
+  let winHeight = 0
+  let y = 0
+  let progress = 0
+  let docHeight = 0
+  let mainClasses = ''
 
-  mainClasses = $page.path === "/events" ? "events" : "";
+  mainClasses = $page.path === '/events' ? 'events' : ''
 
   $: if (docHeight > winHeight) {
-    progress = y / (docHeight - winHeight);
+    progress = y / (docHeight - winHeight)
   }
 </script>
 
@@ -84,8 +84,29 @@
 
 <svelte:head>
   <style src="../styles/global.scss">
-
   </style>
+  <!-- Matomo -->
+  <script type="text/javascript">
+    var _paq = (window._paq = window._paq || [])
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['requireCookieConsent'])
+    _paq.push(['trackPageView'])
+    _paq.push(['enableLinkTracking'])
+    ;(function () {
+      var siteId = process.env.NODE_ENV === 'production' ? 2 : 1
+      var u = '//matomo.love-foundation.org/'
+      _paq.push(['setTrackerUrl', u + 'matomo.php'])
+      _paq.push(['setSiteId', siteId])
+      var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0]
+      g.type = 'text/javascript'
+      g.async = true
+      g.src = u + 'matomo.js'
+      s.parentNode.insertBefore(g, s)
+    })()
+  </script>
+  <!-- End Matomo Code -->
 </svelte:head>
 
 <svelte:window bind:scrollY={y} bind:innerHeight={winHeight} />
@@ -94,7 +115,6 @@
 <progress value={progress} />
 
 <div bind:clientHeight={docHeight}>
-
   <main class={mainClasses}>
     <slot />
   </main>
