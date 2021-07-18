14c99b896d9dc54fb01e471d46281b543bebe043
moekify <19408385+moekify@users.noreply.github.com>
2021-07-18 19:57:55 +0200

    Initiate the new sdk and adjusting the fetchItems function

--- src/_directus.js
+++ src/_directus.js
@@ -1,36 +1,48 @@
-import DirectusSDK from '@directus/sdk-js';
+import { Directus } from '@directus/sdk'
 
 // Query all assets on staging/development, but only published on production
 
-let status = process.env.NODE_ENV === "production" ? "published" : "published,under_review,draft";
+let status =
+  process.env.NODE_ENV === 'production'
+    ? ['published']
+    : ['published', 'under_review', 'draft']
+const directus = new Directus(process.env.DIRECTUS_URL)
 
-export const directus = new DirectusSDK({
-  url: process.env.DIRECTUS_URL,
-  project: "_",
-  token: process.env.DIRECTUS_TOKEN
-});
+const authDirectus = async () => {
+  await directus.auth.static(process.env.DIRECTUS_TOKEN)
+}
 
-
-export async function fetchItems(collection = "", fields = "", filter = {}, limit = -1) {
-
+export async function fetchItems(
+  collection = '',
+  fields = '',
+  filter = {},
+  limit = -1
+) {
+  let filters = {
+    ...filter,
+    status: {
+      _in: status,
+    },
+  }
+  await authDirectus()
   try {
-    let raw = await directus.getItems(collection, {
+    let raw = await directus.items(collection).readMany({
       fields: fields,
-      filter: filter,
+      filter: filters,
       limit: limit,
-      status: status
-    });
+    })
     const items = raw.data
     return items
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
+<<<<<<< HEAD
 
 };
 
 export async function fetchFile(passedID) {
   try {
-    let raw = await directus.getFile(passedID.toString())
+    let raw = await api.getFile(passedID.toString())
     const file = raw.data
     return file
   } catch (err) {
@@ -40,3 +52,6 @@ export async function fetchFile(passedID) {
 
 
 
+=======
+}
+>>>>>>> 43d6300... fixup! Initiate the new sdk and adjusting the fetchItems function
