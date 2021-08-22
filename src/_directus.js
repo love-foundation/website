188f6966e214c32b73c172303ecf52e0b80c4ce8
moekify <19408385+moekify@users.noreply.github.com>
2021-08-22 15:09:01 +0200

    Remove redundant fetchFile Function

--- src/_directus.js
+++ src/_directus.js
@@ -31,18 +31,6 @@ export async function fetchItems(collection = "", fields = "", filter = {}, limi
   } catch (err) {
     console.log(err);
   }
-
 };
-
-export async function fetchFile(passedID) {
-  try {
-    let raw = await api.getFile(passedID.toString())
-    const file = raw.data
-    return file
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 
 
