514333e6059b9aff27281597c154afcebb4ff1ee
Mirko <19408385+moekify@users.noreply.github.com>
2021-09-17 01:29:39 +0200

    Revert "Fix project api calls to query correct asset url"
    This reverts commit fbb28b105f9d9dc107cdae0df6efe5c63237af0d.

--- src/routes/projects/[slug].json.js
+++ src/routes/projects/[slug].json.js
@@ -14,7 +14,7 @@ export async function get(req, res, next) {
           id: project.id,
           slug: project.slug,
           name: project.name,
-          imageUrl: project.main_image ? process.env.DIRECTUS_URL + "/assets/" + project.main_image.id : "placeholder_projects.jpeg",
+          imageUrl: project.main_image ? project.main_image.data.full_url : "placeholder_projects.jpeg",
           pillar: project.pillar,
           heroColor: project.hero_background_color || null,
           content: project.content.map(content => ({
