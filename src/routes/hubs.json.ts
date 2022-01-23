bac05d6db2f0a77e2d5fb94c8c77ffc6e0923349
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 20:58:26 +0100

    Rewrite hubs endpoint to directly use SKD for typescript support

--- src/routes/hubs.json.ts
+++ src/routes/hubs.json.ts
@@ -1,20 +1,24 @@
-import { fetchItems } from '../_directus';
+import { directus, status } from '../_directus';
 
 import fakeResponse from '../../cypress/fixtures/hubs.js';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async () => {
-	let hubs;
 	const callApi = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
 
-	if (callApi) {
-		hubs = await fetchItems('hubs', 'id, city, instagram, facebook', {
-			active: 1
-		});
-	} else {
-		hubs = fakeResponse;
-	}
-
+	const hubs = callApi
+		? await directus()
+				.items('hubs')
+				.readMany({
+					fields: 'id, city, instagram, facebook',
+					filter: {
+						active: true,
+						status: {
+							_in: status
+						}
+					}
+				})
+		: fakeResponse;
 	if (hubs) {
 		return {
 			body: hubs
