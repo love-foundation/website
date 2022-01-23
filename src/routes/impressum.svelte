79c43d803996636dc98e467c0d9ebc354e5b0e5d
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 16:26:32 +0100

    Add an impressum page with content served from directus

---
+++ src/routes/impressum.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`impressum.json`)
+      .then((r) => r.json())
+      .then((queriedContent) => {
+        return { queriedContent }
+      })
+  }
+</script>
+
+<script>
+  import Content from '../components/UI/Content.svelte'
+  export let queriedContent
+</script>
+
+<svelte:head>
+  <title>Love Foundation e.V. - Impressum</title>
+</svelte:head>
+<section id="impressum" class="pad--bottom--small">
+  <Content {queriedContent} />
+</section>
