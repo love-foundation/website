f4c5cc3d8f61cf79f5b4b2bc976cafa39ed33a9e
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 18:42:47 +0100

    Update application structure and remove redundant files

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,16 +1,40 @@
-<script context="module">
-	/** @type {import('@sveltejs/kit').ErrorLoad} */
-	export function load({ error, status }) {
-		return {
-			props: {
-				title: `${status}: ${error.message}`
-			}
-		};
-	}
-</script>
-
 <script>
-	export let title;
+	export let status;
+	export let error;
+
+	const dev = process.env.NODE_ENV === 'development';
 </script>
 
-<h1>{title}</h1>
+<style>
+	h1, p {
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<h1>{status}</h1>
+
+<p>{error.message}</p>
+
+{#if dev && error.stack}
+	<pre>{error.stack}</pre>
+{/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,44 +1,66 @@
-<script lang="ts">
-	import Nav from '$lib/components/UI/Nav.svelte';
-	import Footer from '$lib/components/UI/Footer.svelte';
-	import { page } from '$app/stores';
-	import CookieBar from '$lib/components/UI/CookieBar.svelte';
-	import '../app.scss';
+<script>
+  import Nav from '../components/UI/Nav.svelte'
+  import Footer from '../components/UI/Footer.svelte'
+  import { stores } from '@sapper/app'
+  import CookieBar from '../components/UI/CookieBar.svelte'
 
-	export let segment;
+  const { page } = stores()
 
-	let winHeight = 0;
-	let y = 0;
-	let progress = 0;
-	let docHeight = 0;
-	let mainClasses = '';
-	let progressClasses = '';
+  export let segment
 
-	mainClasses = $page.url.pathname === '/events' ? 'events' : '';
+  let winHeight = 0
+  let y = 0
+  let progress = 0
+  let docHeight = 0
+  let mainClasses = ''
+  let progressClasses = ''
 
-	$: progressClasses =
-		$page.url.pathname === '/events' || $page.url.pathname === '/artists' ? 'background' : '';
+  mainClasses = $page.path === '/events' ? 'events' : ''
 
-	$: if (docHeight > winHeight) {
-		progress = y / (docHeight - winHeight);
-	}
+  $: progressClasses =
+    $page.path === '/events' || $page.path === '/artists' ? 'background' : ''
 
-	let siteId = process.env.NODE_ENV === 'production' ? 2 : 1;
+  $: if (docHeight > winHeight) {
+    progress = y / (docHeight - winHeight)
+  }
+
+  let siteId = process.env.NODE_ENV === 'production' ? 2 : 1
 </script>
 
 <svelte:head>
-	<!-- Matomo -->
-
-	<noscript
-		><p>
-			<img
-				src={`//matomo.love-foundation.org/matomo.php?idsite=${siteId}&amp;rec=1`}
-				style="border:0;"
-				alt=""
-			/>
-		</p></noscript
-	>
-	<!-- End Matomo Code -->
+  <style src="../styles/global.scss">
+  </style>
+  <!-- Matomo -->
+  <script type="text/javascript">
+    var _paq = (window._paq = window._paq || [])
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['requireCookieConsent'])
+    _paq.push(['trackPageView'])
+    _paq.push(['enableLinkTracking'])
+    ;(function () {
+      var siteId = process.env.NODE_ENV === 'production' ? 2 : 1
+      var u = '//matomo.love-foundation.org/'
+      _paq.push(['setTrackerUrl', u + 'matomo.php'])
+      _paq.push(['setSiteId', siteId])
+      var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0]
+      g.type = 'text/javascript'
+      g.async = true
+      g.src = u + 'matomo.js'
+      s.parentNode.insertBefore(g, s)
+    })()
+  </script>
+  <noscript
+    ><p>
+      <img
+        src={`//matomo.love-foundation.org/matomo.php?idsite=${siteId}&amp;rec=1`}
+        style="border:0;"
+        alt=""
+      />
+    </p></noscript
+  >
+  <!-- End Matomo Code -->
 </svelte:head>
 
 <svelte:window bind:scrollY={y} bind:innerHeight={winHeight} />
@@ -47,73 +69,73 @@
 <progress value={progress} class={progressClasses} />
 
 <div bind:clientHeight={docHeight}>
-	<main class={mainClasses}>
-		<slot />
-	</main>
-	<Footer />
+  <main class={mainClasses}>
+    <slot />
+  </main>
+  <Footer />
 </div>
 
 <CookieBar />
 
 <style lang="scss">
-	main {
-		position: relative;
-		max-width: $max-width;
-		background-color: white;
-		padding: 0.5em;
-		padding-bottom: 0;
-		margin: 0 auto;
-		box-sizing: border-box;
-		margin-top: $desktop-header-height;
+  main {
+    position: relative;
+    max-width: $max-width;
+    background-color: white;
+    padding: 0.5em;
+    padding-bottom: 0;
+    margin: 0 auto;
+    box-sizing: border-box;
+    margin-top: $desktop-header-height;
 
-		@include desktop {
-			padding: 2em;
-			padding-bottom: 0;
-		}
-		&.events {
-			padding: unset !important;
-		}
-	}
+    @include desktop {
+      padding: 2em;
+      padding-bottom: 0;
+    }
+    &.events {
+      padding: unset !important;
+    }
+  }
 
-	progress {
-		top: 0;
-		position: fixed;
-		width: 100%;
-		height: 5px;
-		border: none;
-		background: transparent;
-		z-index: 1000;
-		-webkit-appearance: none !important;
+  progress {
+    top: 0;
+    position: fixed;
+    width: 100%;
+    height: 5px;
+    border: none;
+    background: transparent;
+    z-index: 1000;
+    -webkit-appearance: none !important;
 
-		&[value]::-webkit-progress-value {
-			background: $black !important;
-		}
+    &[value]::-webkit-progress-value {
+      background: $black !important;
+    }
 
-		&[value]::-webkit-progress-value {
-			background: $black !important;
-		}
+    &[value]::-webkit-progress-value {
+      background: $black !important;
+    }
 
-		&::-webkit-progress-bar {
-			background-color: none !important;
-			background: none !important;
-			border: none !important;
-			-webkit-appearance: none !important;
-		}
+    &::-webkit-progress-bar {
+      background-color: none !important;
+      background: none !important;
+      border: none !important;
+      -webkit-appearance: none !important;
+    }
 
-		&::-moz-progress-bar {
-			background: none !important;
-			border: none !important;
-		}
+    &::-moz-progress-bar {
+      background: none !important;
+      border: none !important;
+    }
 
-		&[value]::-moz-progress-bar {
-			background: $black !important;
-		}
+    &[value]::-moz-progress-bar {
+      background: $black !important;
+    }
 
-		&.background {
-			background: $medium-grey !important;
-			&::-webkit-progress-bar {
-				background: $medium-grey !important;
-			}
-		}
-	}
+    &.background {
+      background: $medium-grey !important;
+      &::-webkit-progress-bar {
+        background: $medium-grey !important;
+      }
+    }
+  }
 </style>
