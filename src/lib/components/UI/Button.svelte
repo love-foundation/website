2cb21daa6c2dd03a3ed4475a7672c45631550337
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 20:19:56 +0100

    make use of sveltekit:prefetch

--- src/lib/components/UI/Button.svelte
+++ src/lib/components/UI/Button.svelte
@@ -1,33 +1,33 @@
 <script>
-  export let link = "";
-  export let newTab = false;
+	export let link = '';
+	export let newTab = false;
 
-  let target = newTab ? "_blank" : "_self";
+	let target = newTab ? '_blank' : '_self';
 </script>
 
-<style lang="scss">
-  .btn {
-    background-color: $blue;
-    color: #fff;
-    font-weight: 300;
-    display: inline-block;
-    padding: 10px 30px;
-    margin: 0 auto;
-    border-radius: 200px;
-    text-decoration: none;
-    font-size: 150%;
-    &:hover {
-      background-color: $hover-blue;
-    }
-    &--wrap {
-      text-align: center;
-      width: 100%;
-    }
-  }
-</style>
-
 <div class="btn--wrap">
-  <a rel="prefetch" target={target} class="btn" href={link}>
-    <slot />
-  </a>
+	<a sveltekit:prefetch {target} class="btn" href={link}>
+		<slot />
+	</a>
 </div>
+
+<style lang="scss">
+	.btn {
+		background-color: $blue;
+		color: #fff;
+		font-weight: 300;
+		display: inline-block;
+		padding: 10px 30px;
+		margin: 0 auto;
+		border-radius: 200px;
+		text-decoration: none;
+		font-size: 150%;
+		&:hover {
+			background-color: $hover-blue;
+		}
+		&--wrap {
+			text-align: center;
+			width: 100%;
+		}
+	}
+</style>
