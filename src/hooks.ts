0eb3947780b927a9630f273330fba202b17674a7
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 18:54:21 +0100

    Refactor how styles are handled and replace server.js

---
+++ src/hooks.ts
@@ -0,0 +1,32 @@
+import cookie from 'cookie';
+import type { GetSession, Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+
+	const response = await resolve(event);
+
+	if (!cookies.cookieConsent) {
+		// if this is the first time the user has visited this app,
+		// set a cookie so that we recognise them when they return
+		response.headers.set(
+			'set-cookie',
+			cookie.serialize(
+				'cookieConsent',
+				{ necessary: false },
+				{
+					path: '/',
+					httpOnly: true
+				}
+			)
+		);
+	}
+
+	return response;
+};
+
+export const getSession: GetSession = async (request) => {
+	return {
+		events: []
+	};
+};
