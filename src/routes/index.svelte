1af5ff901b58149efc40d5ccc6108be0daa867cb
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 16:37:45 +0100

    Fix a typo on main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <script context="module">
   export function preload({ params, query }) {
     return this.fetch(`hubs.json`)
-      .then(r => r.json())
-      .then(hubs => {
-        return { hubs };
+      .then((r) => r.json())
+      .then((hubs) => {
+        return { hubs }
       })
-      .then(this.fetch('sitemap.xml'));
+      .then(this.fetch('sitemap.xml'))
   }
 </script>
 
@@ -21,7 +21,7 @@
   const { session } = stores()
 
   let upcomingEvents = $session.events.filter(
-    event => new Date(event.starttime) >= new Date()
+    (event) => new Date(event.starttime) >= new Date()
   )
 
   let image = {
@@ -29,6 +29,63 @@
     alt: 'Map of Love Foundation Hubs',
   }
 </script>
+
+<svelte:head>
+  <title>Love Foundation - Home</title>
+</svelte:head>
+<section id="opener" class="pad--bottom--small">
+  <h1>What is Love Foundation?</h1>
+  <div class="row">
+    <figure class="image">
+      <object data="whatislove.svg" type="image/svg+xml" title="Map" />
+    </figure>
+    <h2>
+      Love Foundation is a community of activists and artists spreading love,
+      connecting people and raising money for water development projects through
+      cultural events.
+    </h2>
+    <Button link={'join'}>Join the Family</Button>
+  </div>
+</section>
+
+{#if upcomingEvents.length}
+  <section class="pad--bottom--small">
+    <h1 class="centered">Upcoming Events</h1>
+    <UpcomingEvents {upcomingEvents} />
+  </section>
+{/if}
+
+<section class="love-locations pad--bottom--small">
+  <div class="row">
+    <h1>Where is the Love?</h1>
+
+    <!-- <FullWidthModule {image} /> -->
+    <figure class="image">
+      <object data="map.svg" type="image/svg+xml" title="Map" />
+    </figure>
+
+    <h2 class="main-text">
+      Love Foundation was founded in 2013 by two students from Maastricht
+      University and a designer in the Netherlands.
+      <br />
+      We have organised events in the Netherlands, the US, Australia, Brazil, Israel,
+      Spain, Germany, the UK, Italy, Portugal, Israel, South Africa and France.
+    </h2>
+  </div>
+</section>
+
+<section class="active-hubs row">
+  <h1>Active Love Hubs</h1>
+  <div class="columns is-multiline">
+    {#each hubs as hub}
+      <Hub {hub} />
+    {/each}
+  </div>
+</section>
+<!-- Fix Me: Should be UI component, with properties for link and text -->
+<section class="cta pad--bottom--small centered">
+  <Button link={'join'}>Join the Family</Button>
+</section>
 
 <style lang="scss">
   .centered {
@@ -78,62 +135,3 @@
     }
   }
 </style>
-
-<svelte:head>
-  <title>Love Foundation - Home</title>
-</svelte:head>
-<section id="opener" class="pad--bottom--small">
-  <h1>What is Love Foundation?</h1>
-  <div class="row">
-    <figure class="image">
-      <object data="whatislove.svg" type="image/svg+xml" title="Map" />
-    </figure>
-    <h2>
-      Love Foundation is a community of activists and artists spreading love,
-      connecting people and raising money for water development projects through
-      cultural events.
-    </h2>
-    <Button link={'join'}>Join the Family</Button>
-  </div>
-</section>
-
-{#if upcomingEvents.length}
-  <section class="pad--bottom--small">
-    <h1 class="centered">Upcoming Events</h1>
-    <UpcomingEvents {upcomingEvents} />
-  </section>
-{/if}
-
-<section class="love-locations pad--bottom--small">
-  <div class="row">
-
-    <h1>Where is the Love?</h1>
-
-    <!-- <FullWidthModule {image} /> -->
-    <figure class="image">
-      <object data="map.svg" type="image/svg+xml" title="Map" />
-    </figure>
-
-    <h2 class="main-text">
-      Love Foundation was founded in 2013 by two students from Maastricht
-      University and a designer in the Netherlands.
-      <br />
-      We have organised events in the Netherlands, the US, Australia, Brazil,
-      Israel, Spain, Germany, the UK, Italy, Portugal, Israel, South Africa and
-      France.
-    </h2>
-  </div>
-</section>
-
-<section class="active-hubs row">
-  <h1>Active Love Hubs</h1>
-  <div class="columns is-multiline">
-    {#each hubs as hub}
-      <Hub {hub} />
-    {/each}
-  </div>
-</section>
-<!-- Fix Me: Should be UI component, with properties for link and text -->
-<section class="cta pad--bottom--small centered">
-  <Button link={'join'}>Join the Family</Button>
-</section>
