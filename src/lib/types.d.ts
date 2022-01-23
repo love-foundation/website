b4943303d12048b3ba41ef2f81beb4ffb882d226
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 20:34:03 +0100

    Add a type for Content

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,5 +1,19 @@
-export interface Hubs = {
+export interface Hubs {
 	city: string;
 	facebook: string;
 	instagram: string;
-};
+}
+
+export interface Content {
+	id: string;
+	type: string;
+	details: {
+		text: string;
+		textLayout: string;
+		imageOne: string;
+		imageTwo: string;
+		padding: string;
+		captions: string;
+		heroColor: string | undefined;
+	};
+}
