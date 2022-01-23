c7b7e751818ea296c0c59b61af6ec8fae9213305
Mirko Nitschke <mnitschke93@gmail.com>
2022-01-23 23:05:36 +0100

    Change the content module to accept only ContentCollections and move directus to lib

--- src/lib/components/UI/Content.svelte
+++ src/lib/components/UI/Content.svelte
@@ -1,61 +1,61 @@
-<script>
-  import TextModule from "./ContentModules/TextModule.svelte";
-  import TextImageModule from "./ContentModules/TextImageModule.svelte";
-  import InlineImageModule from "./ContentModules/InlineImageModule.svelte";
-  import HeroModule from "./ContentModules/HeroModule.svelte";
-  import { flip } from "svelte/animate";
-  import { onMount } from "svelte";
-  import lozad from "lozad";
+<script lang="ts">
+	import TextModule from './ContentModules/TextModule.svelte';
+	import TextImageModule from './ContentModules/TextImageModule.svelte';
+	import InlineImageModule from './ContentModules/InlineImageModule.svelte';
+	import HeroModule from './ContentModules/HeroModule.svelte';
+	import { flip } from 'svelte/animate';
+	import { onMount } from 'svelte';
+	import lozad from 'lozad';
+	import type { ContentCollection } from '$lib/types';
+	import type { ArrayElement } from 'src/global';
 
-  export let queriedContent;
+	export let queriedContent: ContentCollection[];
 
-  let layout;
-  let moduleMapping = [
-    { type: "text_only", component: TextModule },
-    { type: "image_left", component: TextImageModule },
-    { type: "image_only", component: InlineImageModule },
-    { type: "double_image", component: InlineImageModule },
-    { type: "image_right", component: TextImageModule },
-    { type: "full_width", component: InlineImageModule },
-    { type: "full_bleed_hero", component: HeroModule }
-  ];
+	const availableContentModules = Object.freeze([
+		TextModule,
+		TextImageModule,
+		InlineImageModule,
+		HeroModule
+	]);
+	const moduleMapping = Object.freeze(
+		new Map<string, ArrayElement<typeof availableContentModules>>([
+			['text_only', TextModule],
+			['image_left', TextImageModule],
+			['image_only', InlineImageModule],
+			['double_image', InlineImageModule],
+			['image_right', TextImageModule],
+			['full_width', InlineImageModule],
+			['full_bleed_hero', HeroModule]
+		])
+	);
 
-  onMount(() => {
-    const observer = lozad();
-    observer.observe();
-  });
-
-  queriedContent.map(qc => {
-    layout = moduleMapping.filter(m => m.type == qc.type);
-    if (layout[0]) {
-      qc.component = layout[0].component;
-    }
-  });
+	onMount(() => {
+		const observer = lozad();
+		observer.observe();
+	});
 </script>
 
-<style lang="scss">
-  section {
-    margin-top: -50px;
-    opacity: 0;
-    transition: margin-top 1s cubic-bezier(0.4, 0.07, 0.32, 0.94),
-      opacity 1s ease-in;
-    &.loaded {
-      opacity: 1;
-      margin-top: 0;
-    }
-  }
-</style>
-
 {#each queriedContent as content, i (content.id)}
-  <section
-    data-toggle-class="loaded"
-    animate:flip
-    class:loaded={i < 3}
-    class:lozad={i >= 3}
-    class:is-centered={content.type != 'full_bleed_hero'}
-    class={`content columns pad--bottom--${content.details.padding}`}>
-    {#if content.component}
-      <svelte:component this={content.component} {content} lazy={i >= 3} />
-    {/if}
-  </section>
+	<section
+		data-toggle-class="loaded"
+		animate:flip
+		class:loaded={i < 3}
+		class:lozad={i >= 3}
+		class:is-centered={content.type != 'full_bleed_hero'}
+		class={`content columns pad--bottom--${content.distance_to_next}`}
+	>
+		<svelte:component this={moduleMapping.get(content.type)} {content} lazy={i >= 3} />
+	</section>
 {/each}
+
+<style lang="scss">
+	section {
+		margin-top: -50px;
+		opacity: 0;
+		transition: margin-top 1s cubic-bezier(0.4, 0.07, 0.32, 0.94), opacity 1s ease-in;
+		&.loaded {
+			opacity: 1;
+			margin-top: 0;
+		}
+	}
+</style>
