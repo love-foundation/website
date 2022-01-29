38f977e455e9b3241a16734082bc852260cf774b
Mirko <mirko@love-foundation.org>
2022-01-29 19:21:18 +0100

    Simplify fakeResponse handling

--- src/routes/about/index.json.ts
+++ src/routes/about/index.json.ts
@@ -1,35 +1,31 @@
-import { directus, status } from '$lib/_directus';
+import { directus, status, callApi } from '$lib/_directus';
 import type { RequestHandler } from '@sveltejs/kit';
+import fs from 'fs';
 
 import fakeResponse from '../../../cypress/fixtures/about.js';
 
 export const get: RequestHandler = async () => {
-	const callApi =
-		import.meta.env.NODE_ENV === 'production' || import.meta.env.NODE_ENV === 'staging';
-	if (callApi) {
-		const aboutPage = await directus()
-			.items('pages')
-			.readMany({
-				fields: 'content.*.*.*',
-				filter: {
-					slug: 'about',
-					status: {
-						_in: status
+	const aboutPage = callApi
+		? await directus()
+				.items('pages')
+				.readMany({
+					fields: 'content.*.*.*',
+					filter: {
+						slug: 'about',
+						status: {
+							_in: status
+						}
 					}
-				}
-			});
-
-		const aboutPageContent = aboutPage.data[0].content;
-		if (aboutPageContent) {
-			return {
-				body: aboutPageContent
-			};
-		}
-
+				})
+		: fakeResponse;
+	const aboutPageContent = aboutPage.data[0].content;
+	if (aboutPageContent) {
 		return {
-			status: 404
+			body: aboutPageContent
 		};
-	} else {
-		return { body: JSON.stringify(fakeResponse) };
 	}
+
+	return {
+		status: 404
+	};
 };
