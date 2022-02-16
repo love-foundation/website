cd226d509917011c7a0e7f39f483250d29960080
Mirko <mirko@love-foundation.org>
2022-02-16 10:00:16 +0100

    Fix images on join page

--- src/routes/join/index.svelte
+++ src/routes/join/index.svelte
@@ -9,13 +9,13 @@
 <h1 class="pad--bottom--small centered">Join the Family</h1>
 
 <h2 class="centered">Download membership application for Love Foundation e.V. now</h2>
-<Button newTab={true} link={'Mitgliedsantrag_LF-eV.pdf'}>Download</Button>
+<Button newTab={true} link={'/Mitgliedsantrag_LF-eV.pdf'}>Download</Button>
 
 <section class="columns pad--bottom--small">
 	<div class="column is-4">
 		<div class="bubble">
 			<figure class="image">
-				<object data="join_family.svg" type="image/svg+xml" title="Map" />
+				<object data="/join_family.svg" type="image/svg+xml" title="Map" />
 			</figure>
 		</div>
 		<Button newTab={false} link={'/join/internships'}>Work with us</Button>
@@ -23,7 +23,7 @@
 	<div class="column is-4">
 		<div class="bubble">
 			<figure class="image">
-				<object data="join_events.svg" type="image/svg+xml" title="Map" />
+				<object data="/join_events.svg" type="image/svg+xml" title="Map" />
 			</figure>
 		</div>
 		<Button newTab={true} link={'events'}>Join our events</Button>
@@ -31,7 +31,7 @@
 	<div class="column is-4">
 		<div class="bubble">
 			<figure class="image">
-				<object data="join_financial.svg" type="image/svg+xml" title="Map" />
+				<object data="/join_financial.svg" type="image/svg+xml" title="Map" />
 			</figure>
 		</div>
 		<Button link={'support'}>Support us financially</Button>
