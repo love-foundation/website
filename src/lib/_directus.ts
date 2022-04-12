a5c6088e315cdada43a07081fc283dd161e3a54d
Mirko <mirko@love-foundation.org>
2022-04-12 23:22:21 +0200

    Override status query for staging

--- src/lib/_directus.ts
+++ src/lib/_directus.ts
@@ -1,12 +1,13 @@
 import type { CustomDirectusTypes } from '$lib/types';
-import { Directus, PartialItem } from '@directus/sdk';
+import { Directus } from '@directus/sdk';
 
 export const callApi = import.meta.env.MODE === 'production';
 
 // Query all assets on staging/development, but only published on production
-
 export const status =
-	import.meta.env.MODE === 'production' ? ['published'] : ['published', 'under_review', 'draft'];
+	import.meta.env.VITE_APP_MODE === 'staging' || import.meta.env.MODE === 'development'
+		? ['published', 'under_review', 'draft']
+		: ['published'];
 const directusSDK = new Directus<CustomDirectusTypes>(
 	import.meta.env.VITE_DIRECTUS_URL.toString(),
 	{
