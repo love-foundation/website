c08c2f3183ab2efb1c573a3aa6904719625d03b1
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-10-10 20:23:15 +0200

    Add matomo back

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -26,7 +26,25 @@
 
 <svelte:head>
 	<!-- Matomo -->
-
+	<script>
+		var _paq = (window._paq = window._paq || []);
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
+		_paq.push(['setDomains', ['*.www.love-foundation.org', '*.love-foundation.org']]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function () {
+			var u = '//matomo.love-foundation.org/';
+			_paq.push(['setTrackerUrl', u + 'matomo.php']);
+			_paq.push(['setSiteId', siteId]);
+			var d = document,
+				g = d.createElement('script'),
+				s = d.getElementsByTagName('script')[0];
+			g.async = true;
+			g.src = u + 'matomo.js';
+			s.parentNode.insertBefore(g, s);
+		})();
+	</script>
 	<noscript
 		><p>
 			<img
