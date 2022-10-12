54acdf8f51d1d7bfab41bb2639fe51a499e0c551
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-10-12 23:27:01 +0200

    Sort the lovecasts based on their episodeNumber
    This is a bigger rework of the fetched data and prepares for also having radiocasts.

--- src/routes/lovecasts/index.json.ts
+++ src/routes/lovecasts/index.json.ts
@@ -3,13 +3,14 @@ import fakeResponse from '../../../cypress/fixtures/lovecasts';
 import type { RequestHandler } from '@sveltejs/kit';
 import type { ConvertedLovecast } from './_types.js';
 
+const episodeNumberRegex = /(\w+cast)\s+(\d{1,4})/i;
+
 export const get: RequestHandler = async () => {
-	const lovecasts = true
+	const lovecasts = callApi
 		? await directus()
 				.items('lovecast')
-
 				.readByQuery({
-					fields: 'id, name_of_the_set, design, soundcloud_link',
+					fields: 'id, name_of_the_set, design, soundcloud_link, type',
 					filter: {
 						status: {
 							_in: status
@@ -20,17 +21,22 @@ export const get: RequestHandler = async () => {
 		: fakeResponse;
 
 	const lovecastsData: ConvertedLovecast[] = lovecasts.data
-		.sort((a, b) => b.name_of_the_set.localeCompare(a.name_of_the_set))
 		.map((lovecast) => {
-			const { id, name_of_the_set, design, soundcloud_link } = lovecast;
+			const { id, name_of_the_set, design, soundcloud_link, type } = lovecast;
+			const [_, regexCastType, episodeNumber] = name_of_the_set.match(episodeNumberRegex);
 			return {
 				id,
 				title: name_of_the_set,
 				soundcloud: soundcloud_link,
-				imageUrl: design ? `${import.meta.env.VITE_DIRECTUS_URL}assets/${design}` : null
+				imageUrl: design ? `${import.meta.env.VITE_DIRECTUS_URL}assets/${design}` : null,
+				type,
+				regexCastType,
+				episodeNumber: parseInt(episodeNumber, 10)
 			};
+		})
+		.sort((a, b) => {
+			return b.regexCastType.localeCompare(a.regexCastType) || b.episodeNumber - a.episodeNumber;
 		});
-
 	if (lovecastsData) {
 		return {
 			body: JSON.stringify(lovecastsData)
