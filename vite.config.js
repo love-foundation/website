6b6efe606955b8407b941f31e16eda8548e251a0
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-12-27 22:10:42 +0100

    Move to vite.config.js for sveltekit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+const config = defineConfig({
 	plugins: [sveltekit()],
 	css: {
 		preprocessorOptions: {
@@ -10,6 +10,6 @@ const config = {
 			}
 		}
 	}
-};
+});
 
 export default config;
