d014c57097fb0c2c2d7a5c3896dda1cdc1e845b7
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-12-27 22:12:37 +0100

    Type definitions similar to fresh svelte app

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,12 @@
-/// <reference types="@sveltejs/kit" />
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+declare namespace App {
+	// interface Error {}
+	// interface Locals {}
+	// interface PageData {}
+	// interface Platform {}
+}
+
 export type ArrayElement<ArrayType extends readonly unknown[]> =
 	ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
