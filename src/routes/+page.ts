6df803f6783126da718ea9a7d54438c74a4abacb
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-12-27 22:12:14 +0100

    Rewrite index root page to new style
    This includes fetching events and hubs at a higher level using the respective new endpoints in layout and page

---
+++ src/routes/+page.ts
@@ -0,0 +1,28 @@
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import { callApi, directus, status } from '$lib/_directus';
+import fakeHubResponse from '../fixtures/hubs';
+
+export const load: PageLoad = async ({ fetch }) => {
+	try {
+		const hubs = callApi
+			? await directus()
+					.items('hubs')
+					.readByQuery({
+						fields: ['id', 'city', 'instagram', 'facebook'],
+						filter: {
+							active: true,
+							status: {
+								_in: status
+							}
+						}
+					})
+			: fakeHubResponse;
+
+		return {
+			hubs: hubs.data
+		};
+	} catch (apiError) {
+		throw error(500, `Could not load homepage: ${apiError}`);
+	}
+};
