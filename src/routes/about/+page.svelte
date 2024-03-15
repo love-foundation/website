84789f18e526274f445ee1d9a0731e38c4183fa7
moekify <19408385+moekify@users.noreply.github.com>
2024-03-15 16:32:29 +0000

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+	import Content from '$lib/components/UI/Content.svelte';
+	import type { PageData } from './$types';
+  export let data: PageData
+
+  const queriedContent = data.about;
+
+</script>
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+{#if queriedContent}
+  <Content {queriedContent} />
+{/if}
---
+++ src/routes/about/+page.ts
@@ -0,0 +1,26 @@
+import { directus, status } from '$lib/_directus';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from '../$types';
+
+export const load: PageLoad = async () => {
+	try {
+		const pageContent = await directus()
+			.items('pages')
+			.readByQuery({
+				fields: ['content.*.*'],
+				filter: {
+					slug: 'about',
+					status: {
+						_in: status
+					}
+				}
+			});
+		if (pageContent.data?.length === 1) {
+			return {
+				about: pageContent.data[0].content
+			};
+		}
+	} catch (apiError) {
+		throw error(500, `Could not load impressum: ${apiError}`);
+	}
+};
