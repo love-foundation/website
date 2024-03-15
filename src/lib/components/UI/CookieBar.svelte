7aeaf85d5f1955078bc6beb57ceb756af6049bd0
moekify <19408385+moekify@users.noreply.github.com>
2024-03-15 23:37:03 +0000

    Fix linter and other configs

---
+++ src/lib/components/UI/CookieBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '@beyonk/gdpr-cookie-consent-banner/dist/style.css'; // optional, you can also define your own styles
+	import GdprBanner from '@beyonk/gdpr-cookie-consent-banner';
+
+	const choices = {
+		necessary: {
+			label: 'Necessary cookies',
+			description: 'Used for cookie control and usage of the donation form',
+			value: true
+		},
+		tracking: false,
+		analytics: false,
+		marketing: false
+	};
+</script>
+
+<GdprBanner
+	heading="We want to inform you about our usage of cookies"
+	cookieName="love_foundation_cookieconsent"
+	description="We only use cookies as part of our donation form that is implemented with a third party supplier that sets cookies. We currently do not set any cookies ourselves."
+	{choices}
+/>
