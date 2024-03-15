d32e2fcc74d3b55428a549dbcecddd380f89b5ad
moekify <19408385+moekify@users.noreply.github.com>
2024-03-15 23:37:03 +0000

    Fix linter and other configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,31 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'prettier',
+		'plugin:svelte/recommended',
+		'prettier'
 	],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte4/svelte4' }],
-	settings: {
-		'svelte4/typescript': () => require('typescript')
-	},
-	rules: {},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
