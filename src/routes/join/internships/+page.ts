d15f8167ac123bf564b4d2011ed02bb9358a0013
moekify <19408385+moekify@users.noreply.github.com>
2024-03-15 16:30:07 +0000

    join pages

---
+++ src/routes/join/internships/+page.ts
@@ -0,0 +1,29 @@
+import { directus, status } from '$lib/_directus';
+
+//FIX ME: This should have a fakeResponse from a fixture for the specific join page
+
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	try {
+		const pageContent = await directus()
+			.items('pages')
+			.readByQuery({
+				fields: ['content.*.*'],
+				filter: {
+					slug: 'join',
+					status: {
+						_in: status
+					}
+				}
+			});
+		if (pageContent.data?.length === 1) {
+			return {
+				join: pageContent.data[0].content
+			};
+		}
+	} catch (apiError) {
+		throw error(500, `Could not load impressum: ${apiError}`);
+	}
+};
