1eff31ebdacb309759ca4ce21c41ed9d7b128e36
moekify <19408385+moekify@users.noreply.github.com>
2024-05-26 12:52:10 +0200

    chore: fix ci build

--- src/routes/events/[slug]/+page.server.ts
+++ src/routes/events/[slug]/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load: PageLoad = async function ({ params, parent }) {
+export const load: PageServerLoad = async function ({ params, parent }) {
 	const parentData = await parent();
 	const singleEvent = parentData.events?.find((event) => event.slug === params.slug);
 	return {
