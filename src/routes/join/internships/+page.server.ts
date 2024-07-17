2afdc152ad78008316b2c3c02e9728161157feb5
moekify <19408385+moekify@users.noreply.github.com>
2024-07-17 22:42:25 +0200

    chore: fix other content fetching

--- src/routes/join/internships/+page.server.ts
+++ src/routes/join/internships/+page.server.ts
@@ -1,22 +1,27 @@
 import { directus, status } from '$lib/_directus';
+import { readItems } from '@directus/sdk';
 import { error } from '@sveltejs/kit';
 
 export const load = async () => {
 	try {
-		const pageContent = await directus()
-			.items('pages')
-			.readByQuery({
-				fields: ['content.*.*'],
+		const pageContent = await directus.request(
+			readItems('pages', {
+				fields: [
+					{
+						content: ['*', { image: ['*'], image_two: ['*'] }]
+					}
+				],
 				filter: {
-					slug: 'join',
+					slug: { _eq: 'join' },
 					status: {
 						_in: status
 					}
 				}
-			});
-		if (pageContent.data?.length === 1) {
+			})
+		);
+		if (pageContent?.length === 1) {
 			return {
-				join: pageContent.data[0].content
+				join: pageContent[0].content
 			};
 		}
 	} catch (apiError) {
