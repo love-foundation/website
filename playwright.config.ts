af076c8f418f6f10ac1ab9b3e3843a7513f4901d
moekify <19408385+moekify@users.noreply.github.com>
2025-01-31 18:26:38 +0100

    chore: let playwright run dev server

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,12 +70,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'npm run dev',
+		command: 'npm run dev:test',
 		url: 'http://localhost:5173',
-		reuseExistingServer: !process.env.CI,
-		env: {
-			...process.env,
-			USE_FIXTURES: 'true'
-		}
+		reuseExistingServer: !process.env.CI
 	}
 });
